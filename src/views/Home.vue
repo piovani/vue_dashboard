e6ac559349b3d23bd8f4f2d4f9467fd9ae2c9f17
Allison Piovani <apiovani25@gmail.com>
2020-07-07 21:34:21 -0300

    Add scroll bar at top page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,12 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
@@ -13,6 +19,33 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
-  }
+  },
+  mounted() {
+    const postWrap = document.querySelector(".home");
+
+    const bar = document.createElement("div");
+
+    bar.style.height = "4px";
+    bar.style.width = "500px";
+    bar.style.backgroundColor = "$6633cc";
+    bar.style.position = "fixed";
+    bar.style.top = "0";
+    bar.style.left = "0";
+    bar.style.zIndex = "9999";
+    bar.style.transition = "0.2s";
+
+    document.body.append(bar);
+
+    document.addEventListener("scroll", () => {
+      const textHeight = postWrap.offsetHeight;
+      const pagePositionY =  window.pageYOffset;
+
+      const updateBar = pagePositionY * 100 / textHeight;
+
+      bar.style.width = updateBar + "%";
+
+      console.log(bar.style.width);
+    });
+  },
 }
 </script>
